a0a4f99220a80f1df8459e19e6c954775a70f204
Tarllysson Silva <tarllyssondesigner@gmail.com>
2026-03-03 18:16:35 -0300

    FEAT(checkout): conectaxão de  provedores globais para o fluxo de carrinho e checkout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_cart_mvvm/data/services/cart_api.dart';
+import 'package:flutter_shopping_cart_mvvm/data/services/checkout_api.dart';
+import 'package:flutter_shopping_cart_mvvm/presentation/products/viewmodel/cart_viewmodel.dart';
+import 'package:flutter_shopping_cart_mvvm/presentation/products/viewmodel/checkout_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 import 'presentation/routes/app_router.dart';
@@ -6,8 +10,24 @@ import 'store/cart_store.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => CartStore(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartStore()),
+        Provider(create: (_) => CartApi()),
+        Provider(create: (_) => CheckoutApi()),
+        ChangeNotifierProvider(
+          create: (ctx) => CartViewModel(
+            ctx.read<CartApi>(),
+            ctx.read<CartStore>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => CheckoutViewModel(
+            ctx.read<CheckoutApi>(),
+            ctx.read<CartStore>(),
+          ),
+        ),
+      ],
       child: const ShoppingCartApp(),
     ),
   );
